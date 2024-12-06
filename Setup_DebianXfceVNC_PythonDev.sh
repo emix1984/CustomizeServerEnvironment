e8f162173d389069df4ad261c061f4c71d07b4ac
emix1984 <60463282+emix1984@users.noreply.github.com>
2024-12-06 10:48:58 +0900

    Update Setup_DebianXfceVNC_PythonDev.sh
    加入安装python3

--- Setup_DebianXfceVNC_PythonDev.sh
+++ Setup_DebianXfceVNC_PythonDev.sh
@@ -12,7 +12,7 @@ apt update
 
 # 安装必要的Python开发环境软件包
 echo "2.安装python开发包"
-apt-get -y install openssl build-essential libssl-dev libffi-dev python3-dev python3-pip python3-venv
+apt-get -y install openssl build-essential libssl-dev libffi-dev python3 python3-dev python3-pip python3-venv
 
 # 安装Chromium浏览器和驱动，用于Web开发和测试
 echo "3.安装chromium-driver开发包"
