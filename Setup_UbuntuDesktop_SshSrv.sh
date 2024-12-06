9586e8b41ff80f3de5ce7691aace2963f20376eb
emix1984 <60463282+emix1984@users.noreply.github.com>
2024-12-06 17:22:24 +0900

    Update Setup_UbuntuDesktop_SshSrv.sh
    删除root相关部分

--- Setup_UbuntuDesktop_SshSrv.sh
+++ Setup_UbuntuDesktop_SshSrv.sh
@@ -4,23 +4,15 @@
 # 日期：2024/12/06
 
 # 更新系统软件包列表
-echo "更新系统包管理清单..."
+echo ">>> 更新系统包管理清单..."
 sudo apt update
 
 # 安装OpenSSH服务器
-echo "安装OpenSSH服务器..."
+echo ">>> 安装OpenSSH服务器..."
 sudo apt install -y openssh-server
 
-# 启用root账户
-echo "启用root账户..."
-sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-# 设置root账户密码（交互式）
-echo "设置root账户密码..."
-echo 'root:1234' | sudo chpasswd
-
 # 重新启动SSH服务以应用更改
-echo "重新启动SSH服务..."
+echo "|->>> ! 重新启动SSH服务..."
 sudo systemctl restart ssh
 
-echo "OpenSSH服务器安装并配置完成。"
+echo ">>> OpenSSH服务器安装并配置完成。"
