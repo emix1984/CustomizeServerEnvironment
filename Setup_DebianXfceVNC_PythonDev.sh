da1adeade4f795621271e39456e5fc0f1afa8de3
emix1984 <60463282+emix1984@users.noreply.github.com>
2025-01-14 18:03:39 +0900

    Update Setup_DebianXfceVNC_PythonDev.sh
    区分桌面版本，这样以后可以选择性的安装桌面专用的包

--- Setup_DebianXfceVNC_PythonDev.sh
+++ Setup_DebianXfceVNC_PythonDev.sh
@@ -9,18 +9,29 @@ echo ">>> 配置Python开发环境 - 配置脚本开始执行"
 echo "1.更新系统包管理清单"
 apt update
 
+# 安装其他必要的软件包，如git curl nano net-tools
+echo "2.安装终端系统常用软件包"
+apt-get -y install git curl nano tree net-tools
 
 # 安装必要的Python开发环境软件包
-echo "2.安装python开发包"
+echo "3.安装python开发包"
 apt-get -y install openssl build-essential libssl-dev libffi-dev python3 python3-dev python3-pip python3-venv
 
-# 安装Chromium浏览器和驱动，用于Web开发和测试
-echo "3.安装chromium-driver开发包"
-apt-get -y install chromium chromium-driver
+# 交互式选择是否安装chromium-driver开发包
+echo "4.是否安装chromium-driver开发包？(y/n)"
+read choice
+if [ "$choice" == "y" ]; then
+    echo "安装chromium-driver开发包"
+    apt-get -y install chromium chromium-driver
+fi
 
-# 安装其他必要的软件包，如gedit、gdebi、nano、unzip
-echo "4.安装系统桌面用软件包 - 可选"
-apt-get -y install gedit gdebi nano unzip
+# 交互式选择是否安装系统桌面用软件包
+echo "5.是否安装系统桌面用软件包 - 可选？(y/n)"
+read choice
+if [ "$choice" == "y" ]; then
+    echo "安装系统桌面用软件包 - 可选"
+    apt-get -y install gedit gdebi unzip
+fi
 
 # 清理缓存和临时文件
 echo "**** 清理缓存和临时文件 ****"
