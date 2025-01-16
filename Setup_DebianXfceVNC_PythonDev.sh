3e9814fa2d559d5974df8f6ccb41826ee5d2334f
emix1984 <60463282+emix1984@users.noreply.github.com>
2025-01-16 11:32:15 +0900

    Update Setup_DebianXfceVNC_PythonDev.sh
    优化代码 if部分-if使用方法错误

--- Setup_DebianXfceVNC_PythonDev.sh
+++ Setup_DebianXfceVNC_PythonDev.sh
@@ -7,7 +7,7 @@ echo ">>> 配置Python开发环境 - 配置脚本开始执行"
 
 # 更新系统软件包列表
 echo "1.更新系统包管理清单"
-apt update && clear
+apt update
 
 # 安装其他必要的软件包，如git curl nano net-tools
 echo "2.安装终端系统常用软件包"
@@ -20,21 +20,25 @@ apt-get -y install openssl build-essential libssl-dev libffi-dev python3 python3
 # 交互式选择是否安装chromium-driver开发包
 echo "4.是否安装chromium-driver开发包？(y/n)"
 read choice
-if [ "$choice" = "y" ]; then
+if [[ "$choice" == "y" ]]; then
     echo "安装chromium-driver开发包"
     apt-get -y install chromium chromium-driver
-else
+elif [[ "$choice" == "n" ]]; then
     echo "跳过安装chromium-driver开发包"
+else
+    echo "输入无效，跳过安装chromium-driver开发包"
 fi
 
 # 交互式选择是否安装系统桌面用软件包
 echo "5.是否安装系统桌面用软件包 - 可选？(y/n)"
 read choice
-if [ "$choice" = "y" ]; then
+if [[ "$choice" == "y" ]]; then
     echo "安装系统桌面用软件包 - 可选"
     apt-get -y install gedit gdebi
-else
+elif [[ "$choice" == "n" ]]; then
     echo "跳过安装系统桌面用软件包"
+else
+    echo "输入无效，跳过安装系统桌面用软件包"
 fi
 
 # 清理缓存和临时文件
