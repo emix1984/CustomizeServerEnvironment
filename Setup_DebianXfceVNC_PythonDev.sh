4fed2aa669dad19b095a761388939f4468a98be4
emix1984 <60463282+emix1984@users.noreply.github.com>
2025-01-25 16:16:17 +0900

    Update Setup_DebianXfceVNC_PythonDev.sh
    修改代码逻辑自动安装桌面安装包管理和文字编辑。selenium手动选择的方式。

--- Setup_DebianXfceVNC_PythonDev.sh
+++ Setup_DebianXfceVNC_PythonDev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# docker镜像要求：consol/debian-xfce-vnc
+# docker镜像要求：consol/debian-xfce-vnc:v2.0.3
 # 作者：Emix1984
-# 日期：2025/1/16
+# 日期：2025/1/25
 
 # 定义函数模块
 function update_system_packages() {
@@ -19,26 +19,31 @@ function install_python_dev_packages() {
     apt-get -y install openssl build-essential libssl-dev libffi-dev python3 python3-dev python3-pip python3-venv
 }
 
-function install_chromium_driver() {
-    echo ">>> Option: 是否安装chromium-driver开发包？(y/n)"
-    read choice
-    if [[ "$choice" == "y" ]]; then
-        echo "安装chromium-driver开发包"
-        apt-get -y install chromium chromium-driver
-    else
-        echo "跳过安装chromium-driver开发包"
-    fi
+function install_desktop_packages() {
+    echo ">>> 4.安装系统桌面用软件包"
+    apt-get -y install gedit gdebi
 }
 
-function install_desktop_packages() {
-    echo ">>> Option: 是否安装系统桌面用软件包 - 可选？(y/n)"
-    read choice
-    if [[ "$choice" == "y" ]]; then
-        echo "安装系统桌面用软件包 - 可选"
-        apt-get -y install gedit gdebi
-    else
-        echo "跳过安装系统桌面用软件包"
-    fi
+function install_chromium_driver() {
+    local choice
+    while true; do
+        echo ">>> Option: 是否安装chromium-driver开发包？(1: 安装, 2: 不安装)"
+        read -r choice
+        case "$choice" in
+            1)
+                echo "安装chromium-driver开发包"
+                apt-get -y install chromium chromium-driver
+                return 0  # 安装完成后继续执行后续脚本
+                ;;
+            2)
+                echo "跳过安装chromium-driver开发包，继续执行后续步骤。"
+                return 0  # 跳过安装，直接继续执行后续脚本
+                ;;
+            *)
+                echo "输入错误，请输入1或2。"
+                ;;
+        esac
+    done
 }
 
 function clean_system() {
@@ -52,8 +57,8 @@ echo ">>> 配置Python开发环境 - 配置脚本开始执行"
 update_system_packages
 install_common_packages
 install_python_dev_packages
-install_chromium_driver
 install_desktop_packages
+install_chromium_driver
 clean_system
 
 echo "脚本执行完毕"
